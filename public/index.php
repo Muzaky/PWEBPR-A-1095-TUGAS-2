47dd9ab66a517607a09e76eab4e0d0b3cd171164
Muzaky <afif050503@gmail.com>
2024-04-20 15:46:50 +0700

    salah masukin modal mas hehe

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 require_once 'C:\laragon\www\PWEBPR-A-1095\app\Models\Controller\contact.php'; // Include the Contact class definition
 
 
-$arr = Contact::index(); 
+$arr = Contact::index();
 
 
 
@@ -173,13 +173,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                                         </svg><i>
                                                             <!-- Icon -->
                                                         </i></a>
-                                                    <button onclick="showDelButton(<?= $id ?>)" class="font-medium text-lg bg-transparent rounded-md border-t-2 border-l-2 border-b-4 border-r-4 border-red-600">
+                                                    <button onclick="showDelButton(event, <?= $id ?>)" class="font-medium text-lg bg-transparent rounded-md border-t-2 border-l-2 border-b-4 border-r-4 border-red-600">
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="w-6 h-6">
                                                             <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                         </svg><i>
                                                             <!-- Icon -->
                                                         </i></button>
                                                 </td>
+                                                <div id="delbutton" class="fixed left-0 top-0 bg-black bg-opacity-40 w-screen h-screen flex items-center justify-center opacity-0 hidden 
+                 transition-opacity duration-500">
+                                                    <div class="bg-white rounded shadow-md p-8 w-[25%] gap-5 flex-col overflow-hidden">
+                                                        <div class="flex gap-3">
+                                                            <div class="bg-red-100 rounded-full text-red-600 min-w-10 h-10 flex items-center justify-center">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+                                                                </svg>
+                                                            </div>
+                                                            <div class="flex-grow">
+                                                                <h1 class="font-bold text-lg mb-2 text-gray-700">Menghapus Data</h1>
+                                                                <p class="text-gray-600">Apakah anda ingin menghapus data ?</p>
+                                                            </div>
+                                                        </div>
+                                                        <div class=" mt-3 flex justify-end">
+                                                            <button onclick="hideDelButton()" class="bg-white rounded px-4 py-2 mr-3 text-black cursor-pointer hover:bg-gray-300">Batal</button>
+                                                            <form class="flex" id="deleteForm">
+                                                                <a href="index.php?action=delete&id=<?= $id ?>" class="bg-red-600 hover:bg-red-700 text-white font-semibold px-4 py-2 rounded">Hapus</a>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
                                             </tr>
                                         </tbody>
                                     <?php
@@ -272,28 +295,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
 
     <!--Modal Delete-->
-    <div id="delbutton" class="fixed left-0 top-0 bg-black bg-opacity-40 w-screen h-screen flex items-center justify-center opacity-0 hidden 
-                 transition-opacity duration-500">
-        <div class="bg-white rounded shadow-md p-8 w-[25%] gap-5 flex-col overflow-hidden">
-            <div class="flex gap-3">
-                <div class="bg-red-100 rounded-full text-red-600 min-w-10 h-10 flex items-center justify-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
-                    </svg>
-                </div>
-                <div class="flex-grow">
-                    <h1 class="font-bold text-lg mb-2 text-gray-700">Menghapus Data</h1>
-                    <p class="text-gray-600">Apakah anda ingin menghapus data ?</p>
-                </div>
-            </div>
-            <div class=" mt-3 flex justify-end">
-                <button onclick="hideDelButton()" class="bg-white rounded px-4 py-2 mr-3 text-black cursor-pointer hover:bg-gray-300">Batal</button>
-                <form class="flex" id="deleteForm">
-                    <a href="index.php?action=delete&id=<?= $id ?>" class="bg-red-600 hover:bg-red-700 text-white font-semibold px-4 py-2 rounded">Hapus</a>
-                </form>
-            </div>
-        </div>
-    </div>
 
 
 
@@ -301,6 +302,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     <script>
+        var id = <?php echo $id; ?>;
+
         function showCreateButton() {
             let createbutton = document.getElementById('createbutton')
 
@@ -321,7 +324,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }, 500);
         }
 
-        function showDelButton(id) {
+        function showDelButton(event, id) {
             let delbutton = document.getElementById('delbutton')
             document.getElementById('deleteForm').href = "index.php?action=delete&id=" + id;;
 
